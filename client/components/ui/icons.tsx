b7a38e55116617aac3a2d61a284b090ed273080c
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-04-13 16:00:33 -0700

    refactor(icons): finished refactoring IconSymbol to Ionicons

--- client/components/ui/icons.tsx
+++ client/components/ui/icons.tsx
@@ -1,9 +1,11 @@
+import { StyleProp, TextStyle } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 type IconProps = {
   name: keyof typeof Ionicons.glyphMap;
   size?: number;
   color?: string;
+  style?: StyleProp<TextStyle>;
 };
 
 export const IconSymbol = ({ name, size = 24, color = "black" }: IconProps) => {
